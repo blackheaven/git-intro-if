578e88f0631756493afd253a0e78be9e70c5d4f8
Gautier DI FOLCO <gautier.difolco@gmail.com>
2013-01-21 18:34:59 +0100

    Redimensionnement.

---
+++ diapositives/resize.sh
@@ -0,0 +1,17 @@
+cp ./conclusion/equipe.gif ./conclusion/equipe_.gif && convert ./conclusion/equipe_.gif -resize 200% .gif &
+cp ./conclusion/magie.gif ./conclusion/magie_.gif && convert ./conclusion/magie_.gif -resize 200% .gif 
+cp ./contenu/oh.gif ./contenu/oh_.gif && convert ./contenu/oh_.gif -resize 200% .gif &
+cp ./conflits/zebre.gif ./conflits/zebre_.gif && convert ./conflits/zebre_.gif -resize 200% .gif 
+cp ./conflits/brute.gif ./conflits/brute_.gif && convert ./conflits/brute_.gif -resize 200% .gif &
+cp ./deplacement/saut.gif ./deplacement/saut_.gif && convert ./deplacement/saut_.gif -resize 200% .gif 
+cp ./deplacement/tete.gif ./deplacement/tete_.gif && convert ./deplacement/tete_.gif -resize 200% .gif &
+cp ./introduction/complique.gif ./introduction/complique_.gif && convert ./introduction/complique_.gif -resize 200% .gif 
+cp ./pratique/pratique.gif ./pratique/pratique_.gif && convert ./pratique/pratique_.gif -resize 200% .gif &
+cp ./references/perdu.gif ./references/perdu_.gif && convert ./references/perdu_.gif -resize 200% .gif 
+cp ./references/luke.gif ./references/luke_.gif && convert ./references/luke_.gif -resize 200% .gif &
+cp ./references/tete.gif ./references/tete_.gif && convert ./references/tete_.gif -resize 200% .gif 
+cp ./reflog/saut.gif ./reflog/saut_.gif && convert ./reflog/saut_.gif -resize 200% .gif &
+cp ./reflog/chat.gif ./reflog/chat_.gif && convert ./reflog/chat_.gif -resize 200% .gif 
+cp ./dag/perplex.gif ./dag/perplex_.gif && convert ./dag/perplex_.gif -resize 200% .gif &
+cp ./dag/chat.gif ./dag/chat_.gif && convert ./dag/chat_.gif -resize 200% .gif 
+cp ./dag/cut.gif ./dag/cut_.gif && convert ./dag/cut_.gif -resize 200% .gif &
